1823fc1930bb3b0c04a7f807d1b2bacfa2721771
Martin Georgiev <137400929+martingeorgiev99@users.noreply.github.com>
2024-11-04 17:44:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from app import create_app
 import os
 
-app = create_app()
+app = create_app()  # Initialize the Flask application
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)  # Run the app on all IP addresses at the specified port
